1179cead094e0381dd93c02f06448377e9c304c7
MaulDev <amadmaul13@gmail.com>
2026-08-03 11:22:18 +0700

    update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -5,10 +5,21 @@ window.CONFIG = {
         .querySelector('meta[name="api-base"]')
         ?.getAttribute("content") || "",
 
-    BASE_PATH :
-        (location.hostname === "localhost" || 
-            location.hostname === "127.0.0.1"
-            ? ""
-            : "/map/"
-        )
+    BASE_PATH: (() => {
+        const host = location.hostname;
+
+        if (host === "localhost" || host === "127.0.0.1") {
+            return "";
+        }
+
+        if (host === "mauldev13.github.io") {
+            return "https://mauldev13.github.io/map";
+        }
+
+        if (host.endsWith(".vercel.app")) {
+            return "";
+        }
+
+        return "";
+    })()
 };
